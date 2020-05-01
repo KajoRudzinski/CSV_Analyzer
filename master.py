83a1971a0c7a9dcd5bdac70b6ad699502582b331
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-05-01 13:51:21 +0200

    Print columns and their types (pandas types)

--- master.py
+++ master.py
@@ -6,9 +6,13 @@ my_csv = pd.read_csv('Pushups.csv')
 my_csv_dim =my_csv.shape
 
 print("Info about your csv:")
+print()
 print("Number of rows: \t" + str(my_csv_dim[0]))
 print("Number of columns: \t" + str(my_csv_dim[1]))
 print("Number of cells: \t" + str(my_csv.size))
+print("-" * 40)
 
-
-
+print()
+print("List of columns:")
+print()
+print(my_csv.dtypes)

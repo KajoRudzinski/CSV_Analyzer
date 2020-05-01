94b37597f06359ce9b3b3c35d2a008007ada7942
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-05-01 13:42:08 +0200

    1st general stats working

--- master.py
+++ master.py
@@ -1,1 +1,14 @@
-print("test")
+import pandas as pd
+
+# testing particular csv file
+my_csv = pd.read_csv('Pushups.csv')
+
+my_csv_dim =my_csv.shape
+
+print("Info about your csv:")
+print("Number of rows: \t" + str(my_csv_dim[0]))
+print("Number of columns: \t" + str(my_csv_dim[1]))
+print("Number of cells: \t" + str(my_csv.size))
+
+
+
